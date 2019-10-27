66fae45aa28e00574dc4bfec38efa3c2ced916fd
KuteKetX <38116775+KuteKetX@users.noreply.github.com>
2019-10-26 20:17:48 -0600

    Create TimeInfo.js

---
+++ Debug/TimeInfo.js
@@ -0,0 +1,28 @@
+'use strict';
+
+function RoundNumber(Value, Point = undefined) {
+	if (Point == undefined) {
+		return Math.round(Value);
+	}
+	var Rounded = Value * Math.pow(10, Point);
+	Rounded = Math.round(Rounded);
+	return Rounded / Math.pow(10, Point);
+}
+
+export function update(Value) {
+	let Scene = thisScene;
+	Value = "";
+	let Time = new Date();
+	let Hours = ("00" + Time.getHours()).slice(-2);
+	let Minutes = ("00" + Time.getMinutes()).slice(-2);
+	let Seconds = ("00" + Time.getSeconds()).slice(-2);
+	var TimePerFrame = (RoundNumber(engine.frametime, 3).toString()).slice(3, 5);
+	if (TimePerFrame.length < 2) {
+		TimePerFrame += "0";
+	}
+	Value += "fTimePerFrame | " + TimePerFrame + "\n";
+	Value += "fTimeRunning  | " + RoundNumber(engine.runtime).toString() + "\n";
+	Value += "cTimeOfDay    | " + (Hours + ":" + Minutes + ":" + Seconds).toString();
+	return Value;
+}
+
