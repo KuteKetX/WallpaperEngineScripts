fbfbe08c38689196817f8856d84731bcbe57b96c
KuteKetX <38116775+KuteKetX@users.noreply.github.com>
2019-11-24 11:13:57 -0700

    Update TimeInformation.js

--- Debug/TimeInformation.js
+++ Debug/TimeInformation.js
@@ -1,5 +1,12 @@
 'use strict';
 
+function InEditor() {
+	if (engine.isRunningInEditor()) {
+		return true;
+	}
+	return false;
+}
+
 function RoundNumber(Value, Point = undefined) {
 	if (Point == undefined) {
 		return Math.round(Value);
@@ -12,17 +19,19 @@ function RoundNumber(Value, Point = undefined) {
 export function update(Value) {
 	let Scene = thisScene;
 	Value = "";
-	let Time = new Date();
-	let Hours = ("00" + Time.getHours()).slice(-2);
-	let Minutes = ("00" + Time.getMinutes()).slice(-2);
-	let Seconds = ("00" + Time.getSeconds()).slice(-2);
-	var TimePerFrame = (RoundNumber(engine.frametime, 3).toString()).slice(3, 5);
-	if (TimePerFrame.length < 2) {
-		TimePerFrame += "0";
+	if (!InEditor()) {
+		let Time = new Date();
+		let Hours = ("00" + Time.getHours()).slice(-2);
+		let Minutes = ("00" + Time.getMinutes()).slice(-2);
+		let Seconds = ("00" + Time.getSeconds()).slice(-2);
+		var TimePerFrame = (RoundNumber(engine.frametime, 3).toString()).slice(3, 5);
+		if (TimePerFrame.length < 2) {
+			TimePerFrame += "0";
+		}
+		Value += "fTimePerFrame | " + TimePerFrame + "\n";
+		Value += "fTimeRunning  | " + RoundNumber(engine.runtime).toString() + "\n";
+		Value += "cTimeOfDay    | " + (Hours + ":" + Minutes + ":" + Seconds).toString();
 	}
-	Value += "fTimePerFrame | " + TimePerFrame + "\n";
-	Value += "fTimeRunning  | " + RoundNumber(engine.runtime).toString() + "\n";
-	Value += "cTimeOfDay    | " + (Hours + ":" + Minutes + ":" + Seconds).toString();
 	return Value;
 }
 
