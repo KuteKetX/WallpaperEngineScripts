22d7235846d66467e9c94bc043c665a2e6a8a3ce
KuteKetX <38116775+KuteKetX@users.noreply.github.com>
2019-11-24 11:11:05 -0700

    Update BoolInformation.js

--- Debug/BoolInformation.js
+++ Debug/BoolInformation.js
@@ -10,13 +10,14 @@ function InEditor() {
 export function update(Value) {
 	let Scene = thisScene;
 	Value = "";
-	Value += "bCursorLeftDown | " + (input.cursorLeftDown).toString() + "\n";
-	Value += "bCameraParallax | " + (Scene.cameraparallax).toString() + "\n";
-	Value += "bClearEnabled   | " + (Scene.clearenabled).toString() + "\n";
-	Value += "bCameraShake    | " + (Scene.camerashake).toString() + "\n";
-	Value += "bCameraFade     | " + (Scene.camerafade).toString() + "\n";
-	Value += "bInEditor       | " + InEditor().toString() + "\n";
-	Value += "bBloom          | " + (Scene.bloom).toString();
+	if (!InEditor()) {
+		Value += "bCursorLeftDown | " + (input.cursorLeftDown).toString() + "\n";
+		Value += "bCameraParallax | " + (Scene.cameraparallax).toString() + "\n";
+		Value += "bClearEnabled   | " + (Scene.clearenabled).toString() + "\n";
+		Value += "bCameraShake    | " + (Scene.camerashake).toString() + "\n";
+		Value += "bCameraFade     | " + (Scene.camerafade).toString() + "\n";
+		Value += "bBloom          | " + (Scene.bloom).toString();
+	}
 	return Value;
 }
 
