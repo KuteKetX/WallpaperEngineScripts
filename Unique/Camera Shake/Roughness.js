ce48f0998081e946af2f4a98ee4283c0dbb51538
KuteKetX <38116775+KuteKetX@users.noreply.github.com>
2019-12-31 17:23:24 -0700

    Create Roughness.js

---
+++ Unique/Camera Shake/Roughness.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var g_Audio = engine.registerAudioBuffers(16);
+var g_MinAmount = 0;
+var g_MaxAmount = 1;
+var g_Reduce = 10;
+
+function CalculateAverage(LeftChannel, RightChannel) {
+	var Average = (LeftChannel + RightChannel) * 0.5;
+	return Average;
+}
+
+export function update(Value) {
+	let Scene = thisScene;
+	if (Scene.camerashake) {
+		Value = 0;
+		var Amount = 0;
+		for (var i = g_MinAmount; i < g_MaxAmount; ++i) {
+			Amount += CalculateAverage(g_Audio.left[i], g_Audio.right[i]);
+		}
+		if (g_Reduce != 0) {
+			Value += Amount / g_Reduce;
+		} else {
+			Value += Amount;
+		}
+	}
+	return Value;
+}
