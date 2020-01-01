5360fe06e739f569a7915a50ad44ce0baea0dacf
KuteKetX <38116775+KuteKetX@users.noreply.github.com>
2019-12-31 17:19:09 -0700

    Create RayIntensity.js

---
+++ Unique/Shine/RayIntensity.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var g_Audio = engine.registerAudioBuffers(16);
+var g_MinAmount = 5;
+var g_MaxAmount = 16;
+var g_Reduce = 25;
+
+function CalculateAverage(LeftChannel, RightChannel) {
+	var Average = (LeftChannel + RightChannel) * 0.5;
+	return Average;
+}
+
+export function update(Value) {
+	Value = 0;
+	var Amount = 0;
+	for (var i = g_MinAmount; i < g_MaxAmount; ++i) {
+		Amount += CalculateAverage(g_Audio.left[i], g_Audio.right[i]);
+	}
+	var Volume = Amount;
+	if (g_Reduce != 0) {
+		Value += Amount / g_Reduce;
+	} else {
+		Value += Amount;
+	}
+	return Value;
+}
