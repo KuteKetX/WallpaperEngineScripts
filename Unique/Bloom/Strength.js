9e3134c8e3772b8e2796ebada09b0a320cb98c37
KuteKetX <38116775+KuteKetX@users.noreply.github.com>
2019-12-31 17:24:13 -0700

    Create Strength.js

---
+++ Unique/Bloom/Strength.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var g_Audio = engine.registerAudioBuffers(16);
+var g_MinAmount = 5;
+var g_MaxAmount = 16;
+var g_Reduce = 0;
+
+function CalculateAverage(LeftChannel, RightChannel) {
+	var Average = (LeftChannel + RightChannel) * 0.5;
+	return Average;
+}
+
+export function update(Value) {
+	let Scene = thisScene;
+	if (Scene.bloom) {
+		Value = 0;
+		var Amount = 0;
+		for (var i = g_MinAmount; i < g_MaxAmount; ++i) {
+			Amount += CalculateAverage(g_Audio.left[i], g_Audio.right[i]);
+		}
+		if (g_Reduce != 0) {
+			Value += Amount / g_Reduce;
+		} else {
+			Value += Amount;
+		}
+	}
+	return Value;
+}
